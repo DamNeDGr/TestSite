9f9a5219953879472728cb0edb8c509c916d7b76
DamNeDGr <f89063431393@yandex.ru>
2025-05-23 18:22:06 +0400

    Add poster

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Video from './video/video.MP4'
-
+import Poster from './video/poster.png'
 export default function VideoPlayer() {
   return (
     <video
         className='rounded-3xl'
+        poster={Poster}
         controls
         src={Video}
         width={300}
